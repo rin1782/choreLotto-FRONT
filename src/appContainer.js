547889794ab2f1a2d16b9fcc6811e039b6fe696d
rin1782 <colaerin@yahoo.com>
2021-05-20 17:37:53 -0400

    initial new front

---
+++ src/appContainer.js
@@ -0,0 +1,17 @@
+class AppContainer{
+    chores = []
+    kids = []
+    baseURL = ""
+    randomChore = {}
+
+    getChores(){
+        //make fetch request to /chores
+        //populate chores and kids properties with returned json data
+        //call renderChores
+    }
+
+    renderChores(){
+        //create DOM nodes and insert data into them
+        //to render in the DOM
+    }
+}
---
+++ src/chore.js
@@ -0,0 +1,7 @@
+class Chore{
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+//store which kid each chore belongs to
